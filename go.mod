ad4ec13e3071068d1d9b1e490d346902a38f8dad
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2025-11-23 13:19:17 +0200

    improve config, separate builds and runs, better output

--- go.mod
+++ go.mod
@@ -19,4 +19,5 @@ require (
 	golang.org/x/crypto v0.45.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
+	gonum.org/v1/gonum v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"crypto/sha256"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -8,9 +11,14 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/go-git/go-git/v6"
+	"github.com/go-git/go-git/v6/config"
 	"github.com/go-git/go-git/v6/plumbing"
+	"gonum.org/v1/gonum/stat"
 )
 
 type settings struct {
@@ -28,33 +36,18 @@ type commit struct {
 
 type appConfig struct {
 	BaseSettings settings `json:"baseSettings"`
+	RemoteURL    string   `json:"remote"`
 	Commits      []commit `json:"commits"`
 	Pwd          string
 }
 
-func getConfig() (*appConfig, error) {
-	pwd, err := os.Getwd()
+func dirExists(path string) bool {
+	info, err := os.Stat(path)
 	if err != nil {
-		return nil, err
-	}
-	configPath := path.Join(pwd, "config.json")
-
-	data, err := os.ReadFile(configPath)
-	if err != nil {
-		return nil, err
+		return false
 	}
 
-	var config appConfig
-	if err = json.Unmarshal(data, &config); err != nil {
-		return nil, err
-	}
-	config.Pwd = pwd
-
-	for _, c := range config.Commits {
-		applySettings(&c.Settings, config.BaseSettings)
-	}
-
-	return &config, nil
+	return info.IsDir()
 }
 
 func applySettings(cfgSettings *settings, baseSettings settings) {
@@ -72,64 +65,255 @@ func applySettings(cfgSettings *settings, baseSettings settings) {
 	}
 }
 
-func runCommit(c *commit, dir string) error {
-	commitPath := path.Join(dir, c.Hash)
+func getConfig() (*appConfig, error) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	configPath := path.Join(pwd, "config.json")
 
-	// Clone repo
-	info, err := os.Stat(commitPath)
-	if err != nil || !info.IsDir() {
-		if !errors.Is(err, os.ErrNotExist) {
-			return err
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var appConfig appConfig
+	if err = json.Unmarshal(data, &appConfig); err != nil {
+		return nil, err
+	}
+	appConfig.Pwd = pwd
+
+	for i := range appConfig.Commits {
+		commitSettings := &appConfig.Commits[i].Settings
+		applySettings(commitSettings, appConfig.BaseSettings)
+	}
+
+	return &appConfig, nil
+}
+
+func getHash(h string, cfg *appConfig) (*plumbing.Hash, error) {
+	hash := plumbing.ZeroHash
+
+	if h == "HEAD" {
+		remote := git.NewRemote(nil, &config.RemoteConfig{
+			Name: "origin",
+			URLs: []string{cfg.RemoteURL},
+		})
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		refs, err := remote.ListContext(ctx, &git.ListOptions{})
+		if err != nil {
+			return nil, fmt.Errorf("ListContextError: %v", err)
 		}
 
-		// Clone repo
-		fmt.Printf("Cloning into %s...\n", commitPath)
-		r, err := git.PlainClone(commitPath, &git.CloneOptions{
-			URL:        "https://github.com/LucDeCaf/c-chess",
+		var target plumbing.ReferenceName
+		for _, r := range refs {
+			if r.Name() == plumbing.HEAD {
+				if r.Type() == plumbing.SymbolicReference {
+					target = r.Target()
+				} else {
+					hash = r.Hash()
+				}
+				break
+			}
+		}
+
+		// If head is symbolic reference, find head from target
+		if target != "" {
+			for _, r := range refs {
+				if r.Name() == target {
+					hash = r.Hash()
+					break
+				}
+			}
+		}
+
+		if hash == plumbing.ZeroHash {
+			return nil, fmt.Errorf("Failed to find HEAD commit hash")
+		}
+	} else {
+		var ok bool
+		hash, ok = plumbing.FromHex(h)
+		if !ok {
+			return nil, fmt.Errorf("Failed to parse commit hash: %v", h)
+		}
+	}
+
+	return &hash, nil
+}
+
+func buildCommit(c *commit, cfg *appConfig) error {
+	hash, err := getHash(c.Hash, cfg)
+	if err != nil {
+		return fmt.Errorf("Error getting hash: %v", err)
+	}
+	partialHash := hash.String()[:7]
+
+	commitDir := path.Join(cfg.Pwd, "build", hash.String())
+
+	// Clone repo
+	if !dirExists(commitDir) {
+		fmt.Printf("Cloning into %s...\n", commitDir)
+		r, err := git.PlainClone(commitDir, &git.CloneOptions{
+			URL:        cfg.RemoteURL,
 			NoCheckout: true,
 			Progress:   os.Stdout,
 		})
 		if err != nil {
-			return err
+			return fmt.Errorf("Error cloning repo: %v", err)
 		}
 
 		w, err := r.Worktree()
 		if err != nil {
-			return err
+			return fmt.Errorf("Error getting worktree: %v", err)
 		}
 
-		hash, ok := plumbing.FromHex(c.Hash)
-		if !ok {
-			return fmt.Errorf("Invalid hash: %s", c.Hash)
-		}
 		err = w.Checkout(&git.CheckoutOptions{
-			Hash: hash,
+			Hash: *hash,
 		})
 		if err != nil {
-			return err
+			return fmt.Errorf("Error checking out commit: %v", err)
 		}
-	} else {
-		fmt.Printf("Found commit %s\n", commitPath)
-		fmt.Printf("%+v", c)
 	}
 
-	// Build commit
-	err = os.Chdir(commitPath)
+	// Check SHA of commit settings
+	json, err := json.Marshal(c.Settings)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error marshalling commit settings: %v", err)
 	}
-	cmd := exec.Command("bash", "-c", c.Settings.BuildCmd)
-	err = cmd.Run()
+	fmt.Printf("Checking SHA: %v\n", partialHash)
+	sha := sha256.Sum256(json)
+	shaFile := path.Join(commitDir, "__bench")
+	rebuild := false
+
+	_, err = os.Stat(shaFile)
+	if err == nil {
+		// File exists
+		data, err := os.ReadFile(shaFile)
+		if err != nil {
+			return fmt.Errorf("Error reading SHA file: %v")
+		}
+
+		// Only rebuild if settings have changed
+		rebuild = !bytes.Equal(data, sha[:])
+		if rebuild {
+			fmt.Println("SHA mismatch")
+		} else {
+			fmt.Println("SHA verified")
+		}
+	}
+
+	if rebuild || errors.Is(err, os.ErrNotExist) {
+		err = os.WriteFile(shaFile, sha[:], 0644)
+		if err != nil {
+			return fmt.Errorf("Failed to write SHA: %v", err)
+		}
+		fmt.Println("SHA written")
+
+		rebuild = true
+	} else {
+		return fmt.Errorf("Error with Stat: %v", err)
+	}
+
+	// Build project
+	if rebuild {
+		fmt.Printf("Building %s...\n", partialHash)
+		err = os.Chdir(commitDir)
+		if err != nil {
+			return fmt.Errorf("Error changing dirs: %v", err)
+		}
+		cmd := exec.Command("bash", "-c", c.Settings.BuildCmd)
+		err = cmd.Run()
+		if err != nil {
+			return fmt.Errorf("Error building commit: %v", err)
+		}
+		fmt.Println("Build complete")
+	}
+
+	return nil
+}
+
+func buildCommits(cfg *appConfig) error {
+	buildPath := path.Join(cfg.Pwd, "build")
+	os.MkdirAll(buildPath, os.ModePerm)
+
+	var err error
+	for i := range cfg.Commits {
+		err = buildCommit(&cfg.Commits[i], cfg)
+		if err != nil {
+			log.Fatalf("Build error: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func runCommit(c *commit, cfg *appConfig) error {
+	// Get commit hash and build dir path
+	hash, err := getHash(c.Hash, cfg)
 	if err != nil {
-		return err
+		return fmt.Errorf("Error getting hash: %v", err)
+	}
+	partialHash := hash.String()
+	if len(partialHash) >= 8 {
+		partialHash = partialHash[:7]
+	}
+
+	// Make sure commit is present and built
+	commitDir := path.Join(cfg.Pwd, "build", hash.String())
+	binDir := path.Join(commitDir, "bin")
+	if !dirExists(commitDir) {
+		return fmt.Errorf("Failed to locate commit directory for %v", partialHash)
+	}
+	if !dirExists(binDir) {
+		return fmt.Errorf("Failed to locate bin directory for %v", partialHash)
+	}
+
+	err = os.Chdir(commitDir)
+	if err != nil {
+		return fmt.Errorf("Error changing dirs: %v", err)
 	}
 
 	// Run commit
-	cmd = exec.Command("bash", "-c", "time", c.Settings.RunCmd)
+	var cmd *exec.Cmd
+	runCmd := strings.ReplaceAll(c.Settings.RunCmd, "%p", strconv.Itoa(c.Settings.Depth))
+
+	// Run once to 'warm up' program
+	cmd = exec.Command("bash", "-c", runCmd)
 	err = cmd.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to pre-bench run: %v\n", err)
 	}
+
+	fmt.Printf("Benchmarking %s... (%s)\n", partialHash, c.Label)
+
+	runs := make([]float64, c.Settings.Runs, c.Settings.Runs)
+	for i := 0; i < c.Settings.Runs; i++ {
+		cmd = exec.Command("bash", "-c", runCmd)
+
+		start := time.Now()
+		err = cmd.Run()
+		elapsed := time.Since(start)
+		if err != nil {
+			return fmt.Errorf("Failed to perform run %d: %v\n", i+1, err)
+		}
+
+		// Store milliseconds as micros / 1000 for more precision
+		runs[i] = float64(elapsed.Microseconds()) / 1000.0
+	}
+
+	avgMillis := stat.Mean(runs, nil)
+	stdDev := stat.StdDev(runs, nil)
+
+	// Print results
+	fmt.Printf("Runtimes (n=%d): { %.2f", c.Settings.Runs, runs[0])
+	for _, r := range runs[1:] {
+		fmt.Printf(", %.2f", r)
+	}
+	fmt.Printf(" }\n")
+	fmt.Printf("Average runtime: %.2fms (σ=%.3f)\n", avgMillis, stdDev)
 
 	return nil
 }
@@ -140,9 +324,9 @@ func runCommits(cfg *appConfig) {
 
 	fmt.Println("Running commits")
 	for _, c := range cfg.Commits {
-		err := runCommit(&c, buildPath)
+		err := runCommit(&c, cfg)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("Run error: %v", err)
 		}
 	}
 }
@@ -153,5 +337,6 @@ func main() {
 		log.Fatal(err)
 	}
 
+	buildCommits(config)
 	runCommits(config)
 }
